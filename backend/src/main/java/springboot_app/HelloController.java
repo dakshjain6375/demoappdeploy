11158acd47c3935b920205083618d974045cf768
dakshjain6375 <dakshjain6375@gmail.com>
2026-07-13 11:23:13 +0530

    adding frontend and backend

---
+++ backend/src/main/java/springboot_app/HelloController.java
@@ -0,0 +1,13 @@
+package springboot_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Daksh this side and I completed the deployment of Springboot CI/CD pipeline !!!!!";
+    }
+}
